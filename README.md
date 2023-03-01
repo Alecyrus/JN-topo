3acf0a1d65bfabf89c776e4864f1a67ec284a8df
Alecyrus <alecyrus@thorn.so>
2023-03-01 21:59:04 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JN-topo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-jleqrv)
---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import ForceGraph3D from '3d-force-graph';
+import graphData from './data';
+
+onMounted(() => {
+  const Graph = ForceGraph3D()(document.getElementById('3d-graph'))
+    .nodeAutoColorBy('node_type')
+    .linkAutoColorBy((d) => {
+      console.log(graphData.nodes[d.source]);
+      return graphData.nodes[d.source].node_type;
+    })
+    .linkOpacity(0.5)
+    .graphData(graphData);
+});
+</script>
+
+<template>
+  <div id="3d-graph"></div>
+</template>
+
+<style scoped>
+#3d-graph {
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+}
+#app {
+  padding: 0;
+  margin: 0;
+}
+</style>
